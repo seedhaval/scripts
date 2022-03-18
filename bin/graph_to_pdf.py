d48eeb8417f8675728b565e89e03798abf7acaa8
Dhaval Shah <seedhaval@gmail.com>
2022-03-18 12:15:21 +0530

    adding mind maps

--- bin/graph_to_pdf.py
+++ bin/graph_to_pdf.py
@@ -3,16 +3,15 @@ import pygraphviz as pgv
 import re
 import uuid
 from pathlib import Path
+import os
 
 fldr = Path('/home/dvs/scripts/mind_maps/')
 
 
-def add_node(lbl, clr, G):
+def add_node(lbl,  G):
     nid = str(uuid.uuid4())
     G.add_node(nid)
     n = G.get_node(nid)
-    #n.attr['style'] = 'filled'
-    #n.attr['fillcolor'] = clr
     n.attr['label'] = lbl
     return nid
 
@@ -21,22 +20,26 @@ def process_file(fl):
     with open(fl, 'r') as f:
         data = f.read().strip().splitlines()
 
-    G = pgv.AGraph(directed=True)
-    clr = ['#d7191c', '#fdae61', '#ffffbf', '#abd9e9', '#2c7bb6']
-    root_nid = add_node(data[0], clr[0], G)
-    stack = [root_nid]*5
+    G = pgv.AGraph(directed=True, rankdir='LR')
+
+    root_nid = add_node(data[0],  G)
+    stack = [root_nid]*10
 
     for ln in data[1:]:
-        c_idx = len(re.findall(r'^\s*', ln)[0])//3
+        c_idx = len(re.findall(r'^\s*', ln)[0])//4
         lbl = ln.strip()
         p_idx = c_idx - 1
-        c_nid = add_node(lbl, clr[c_idx], G)
+        c_nid = add_node(lbl,  G)
         stack[c_idx] = c_nid
         G.add_edge(stack[p_idx], c_nid)
 
-    G.layout(prog='sfdp')
-    G.draw('/mnt/c/Users/Dell/Desktop/mind_maps/' +
-           fl.name.replace('.txt', '.svg'))
+    tgt_pth = '/mnt/c/Users/Dell/Desktop/' + \
+        str(fl.parents[0]).replace('/home/dvs/scripts/', '') + '/'
+    if not os.path.exists(tgt_pth):
+        os.makedirs(tgt_pth)
+
+    G.layout(prog='dot')
+    G.draw(tgt_pth + fl.name.replace('.txt', '.pdf'))
 
 
 for fl in fldr.rglob('*.txt'):
