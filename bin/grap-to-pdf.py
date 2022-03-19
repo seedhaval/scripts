8a1d9ace4431597bbfb91411e241e71e12d05bfc
Dhaval Shah <seedhaval@gmail.com>
2022-03-19 11:11:04 +0530

    generating pdf only for new / updated files

--- bin/grap-to-pdf.py
+++ bin/grap-to-pdf.py
@@ -12,11 +12,22 @@ def add_node(lbl,  G):
     nid = str(uuid.uuid4())
     G.add_node(nid)
     n = G.get_node(nid)
-    n.attr['label'] = lbl.replace('\\','\\\\')
+    n.attr['label'] = lbl.replace('\\', '\\\\')
     return nid
 
 
 def process_file(fl):
+    tgt_pth = '/mnt/c/Users/Dell/Desktop/google_drive_bkp/' + \
+        str(fl.parents[0]).replace('/home/dvs/scripts/', '') + '/'
+    if not os.path.exists(tgt_pth):
+        os.makedirs(tgt_pth)
+
+    tgt_fl = tgt_pth + fl.name.replace('.txt', '.pdf')
+    if os.path.exists(tgt_fl) and os.path.getmtime(fl) < os.path.getmtime(tgt_fl):
+        return
+
+    print(f'generating pdf for {fl}')
+
     with open(fl, 'r') as f:
         data = f.read().strip().splitlines()
 
@@ -33,13 +44,8 @@ def process_file(fl):
         stack[c_idx] = c_nid
         G.add_edge(stack[p_idx], c_nid)
 
-    tgt_pth = '/mnt/c/Users/Dell/Desktop/google_drive_bkp/' + \
-        str(fl.parents[0]).replace('/home/dvs/scripts/', '') + '/'
-    if not os.path.exists(tgt_pth):
-        os.makedirs(tgt_pth)
-
     G.layout(prog='dot')
-    G.draw(tgt_pth + fl.name.replace('.txt', '.pdf'))
+    G.draw(tgt_fl)
 
 
 for fl in fldr.rglob('*.txt'):
