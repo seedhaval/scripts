39fe32f0e7968633d3751c01e8253c8d7db65363
Dhaval Shah <seedhaval@gmail.com>
2022-07-16 17:22:09 +0530

    adding code for breathe in breathe out

---
+++ python_scripts/breathe_in_out_speak.py
@@ -0,0 +1,14 @@
+import pyttsx3
+import time
+
+num_cycle=3
+pause_sec=3
+
+engine = pyttsx3.init()
+for i in range(num_cycle):
+    engine.say("breathe in")
+    engine.runAndWait()
+    time.sleep(pause_sec)
+    engine.say("breathe out")
+    engine.runAndWait()
+    time.sleep(pause_sec)
