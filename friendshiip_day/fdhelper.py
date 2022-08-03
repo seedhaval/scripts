c180a802996801bf0d4377c4e34b58ccfaf1d379
Dhaval Shah <seedhaval@gmail.com>
2022-08-03 07:25:31 +0530

    updating friendship day code

--- friendshiip_day/fdhelper.py
+++ friendshiip_day/fdhelper.py
@@ -3,208 +3,207 @@ from typing import List
 
 
 def dummy(*args, **kwargs):
-	pass
+    pass
 
 
 def pos(elm, xy: List[int]):
-	elm.place(x=xy[0], y=xy[1])
+    elm.place(x=xy[0], y=xy[1])
 
 
 class MyWidget:
-	def __init__(self,elm,xy: List[int]):
-		self.elm = elm
-		self.default_bg = self.elm.cget('background')
-		self.x,self.y = xy
-		pos(self.elm,xy=xy)
+    def __init__(self, elm, xy: List[int]):
+        self.elm = elm
+        self.default_bg = self.elm.cget('background')
+        self.x, self.y = xy
+        pos(self.elm, xy=xy)
 
-	def move(self,x:int,y:int):
-		self.x, self.y = x,y
-		pos(self.elm,xy=[x,y])
-		
-	def bg(self,clr:str):
-		self.elm.config(bg=clr)
-		
-	def reset_bg(self):
-		self.elm.config(bg=self.default_bg)
-		
-	def hide(self):
-		self.elm.place_forget()
-		
-	def show(self):
-		pos(self.elm,xy=[self.x,self.y])
-		
-	
+    def move(self, x: int, y: int):
+        self.x, self.y = x, y
+        pos(self.elm, xy=[x, y])
+
+    def bg(self, clr: str):
+        self.elm.config(bg=clr)
+
+    def reset_bg(self):
+        self.elm.config(bg=self.default_bg)
+
+    def hide(self):
+        self.elm.place_forget()
+
+    def show(self):
+        pos(self.elm, xy=[self.x, self.y])
+
 
 class MyButton(MyWidget):
-	def __init__(self, prnt, text: str, cb, xy: List[int]):
-		self.prnt = prnt
-		self.elm = Button(self.prnt, text=text, command=cb)		
-		super().__init__(self.elm,xy)
-		
-	def set(self, text):		
-		self.elm['text'] = text
-		
-class MyDropdown(MyWidget):
-	def __init__(self, prnt, options: List[str], text: str, xy: List[int]):
-		self.prnt = prnt
-		self.var: StringVar = StringVar()
-		self.var.set(text)
-		self.elm = OptionMenu(self.prnt, self.var,*options)					
-		super().__init__(self.elm,xy)
+    def __init__(self, prnt, text: str, cb, xy: List[int]):
+        self.prnt = prnt
+        self.elm = Button(self.prnt, text=text, command=cb)
+        super().__init__(self.elm, xy)
 
-	def set(self, text):
-		self.var.set(text)
-			
+    def set(self, text):
+        self.elm['text'] = text
+
+
+class MyDropdown(MyWidget):
+    def __init__(self, prnt, options: List[str], text: str, xy: List[int]):
+        self.prnt = prnt
+        self.var: StringVar = StringVar()
+        self.var.set(text)
+        self.elm = OptionMenu(self.prnt, self.var, *options)
+        super().__init__(self.elm, xy)
+
+    def set(self, text):
+        self.var.set(text)
+
 
 class MyLabel(MyWidget):
-	def __init__(self, prnt, text: str, width: int, height: int, xy: List[int]):
-		self.prnt = prnt
-		self.var: StringVar = StringVar()
-		self.elm = Label(self.prnt, textvariable=self.var)
-		self.var.set(text)
-		self.elm.config(width=width)
-		self.elm.config(height=height)		
-		super().__init__(self.elm,xy)
+    def __init__(self, prnt, text: str, width: int, height: int, xy: List[int]):
+        self.prnt = prnt
+        self.var: StringVar = StringVar()
+        self.elm = Label(self.prnt, textvariable=self.var)
+        self.var.set(text)
+        self.elm.config(width=width)
+        self.elm.config(height=height)
+        super().__init__(self.elm, xy)
 
-	def set(self, text):
-		self.var.set(text)
-	
+    def set(self, text):
+        self.var.set(text)
+
 
 class MyText(MyWidget):
-	def __init__(self, prnt, text: str, width: int, height: int, xy: List[int]):
-		self.prnt = prnt	   
-		self.elm = Text(self.prnt, width=width, height=height)
-		self.set(text)		
-		super().__init__(self.elm,xy)
+    def __init__(self, prnt, text: str, width: int, height: int, xy: List[int]):
+        self.prnt = prnt
+        self.elm = Text(self.prnt, width=width, height=height)
+        self.set(text)
+        super().__init__(self.elm, xy)
 
-	def clear(self):
-	   self.elm.delete('1.0',END)
-	   
-	def set(self, text):
-		self.clear()
-		self.elm.insert(END,text)
+    def clear(self):
+        self.elm.delete('1.0', END)
+
+    def set(self, text):
+        self.clear()
+        self.elm.insert(END, text)
+
 
 class MyFrame:
-	def __init__(self, prnt, title: str, width: int, height: int, xy: List[int]):
-		self.elm: LabelFrame = LabelFrame(prnt, text=title)
-		self.elm.config(width=width)
-		self.elm.config(height=height)
-		pos(self.elm, xy)
-		self.children = {}
+    def __init__(self, prnt, title: str, width: int, height: int, xy: List[int]):
+        self.elm: LabelFrame = LabelFrame(prnt, text=title)
+        self.elm.config(width=width)
+        self.elm.config(height=height)
+        pos(self.elm, xy)
+        self.children = {}
 
-	def add_label(self, text: str, width: int, height: int, xy: List[int]) -> MyLabel:
-		self.children[text]: MyLabel = MyLabel(self.elm, text, width, height, xy)
-		return self.children[text]
-		
-	def add_text(self, text: str, width: int, height: int, xy: List[int]) -> MyText:
-		self.children[text]: MyText = MyText(self.elm, text, width, height, xy)
-		return self.children[text]
+    def add_label(self, text: str, width: int, height: int, xy: List[int]) -> MyLabel:
+        self.children[text]: MyLabel = MyLabel(self.elm, text, width, height, xy)
+        return self.children[text]
 
-	def add_button(self, text: str, cb, xy: List[int]) -> MyButton:
-		self.children[text]: MyButton = MyButton(self.elm, text, cb, xy)
-		return self.children[text]
+    def add_text(self, text: str, width: int, height: int, xy: List[int]) -> MyText:
+        self.children[text]: MyText = MyText(self.elm, text, width, height, xy)
+        return self.children[text]
 
-	def add_dropdown(self, options: List[str], text: str, xy: List[int]) -> MyDropdown:
-		self.children[text]: MyDropdown = MyDropdown(self.elm, options, text, xy)
-		return self.children[text]
+    def add_button(self, text: str, cb, xy: List[int]) -> MyButton:
+        self.children[text]: MyButton = MyButton(self.elm, text, cb, xy)
+        return self.children[text]
+
+    def add_dropdown(self, options: List[str], text: str, xy: List[int]) -> MyDropdown:
+        self.children[text]: MyDropdown = MyDropdown(self.elm, options, text, xy)
+        return self.children[text]
+
 
 class MyApp:
-	def __init__(self, title: str, width: int, height: int):
-		self.top = Tk()
-		self.top.title(title)
-		self.top.grid_propagate(False)
-		self.top.geometry(f'{width}x{height}+10+10')
-		self.children = {}
-		
+    def __init__(self, title: str, width: int, height: int):
+        self.top = Tk()
+        self.top.title(title)
+        self.top.grid_propagate(False)
+        self.top.geometry(f'{width}x{height}+10+10')
+        self.top.option_add("*font", "verdana 28")
+        self.children = {}
 
-	def add_frame(self, title: str, width: int, height: int, xy: List[int]) -> MyFrame:
-		self.children['title'] = MyFrame(self.top, title, width, height, xy)
-		return self.children['title']
+    def add_frame(self, title: str, width: int, height: int, xy: List[int]) -> MyFrame:
+        self.children['title'] = MyFrame(self.top, title, width, height, xy)
+        return self.children['title']
 
-	def show(self):
-		self.top.mainloop()
+    def show(self):
+        self.top.mainloop()
+
 
 class Scene:
-	def __init__(self,obj,pafter=0):
-		self.obj = obj				
-		self.is_finished = False		
-		self.cur_step = 0
-		self.pafter = pafter
-				
-		
-	def set_move(self, tgt):
-		self.tgt = tgt
-		self.steps = 15
-		self.delay_ms = 200
-		self.init = self.init_move
-		
-	def init_move(self):
-		self.strt = {k:(v.x,v.y) for k,v in self.obj.items()}				
-		self.delta = {}
-		for k,v in self.tgt.items():
-			dx = (self.tgt[k][0]-self.strt[k][0])*1.0/self.steps
-			dy = (self.tgt[k][1]-self.strt[k][1])*1.0/self.steps
-			self.delta[k] = (dx,dy)
-		self.action = self.move
-		
-	def move(self):
-		for k in self.tgt.keys():
-			obj = self.obj[k]
-			dx,dy = self.delta[k]
-			obj.move(obj.x+dx,obj.y+dy)
-		self.cur_step += 1
-		if self.cur_step >= self.steps:
-			self.is_finished = True
+    def __init__(self, obj, pafter=0):
+        self.obj = obj
+        self.is_finished = False
+        self.cur_step = 0
+        self.pafter = pafter
 
-	def set_show_once(self,wdgt,text,dly):
-		self.wdgt = wdgt
-		self.text = text
-		self.delay_ms = dly
-		self.init = dummy
-		self.action = self.show_once
-		
+    def set_move(self, tgt):
+        self.tgt = tgt
+        self.steps = 15
+        self.delay_ms = 200
+        self.init = self.init_move
 
-	def show_once(self):
-		if self.cur_step == 0:
-			self.obj[self.wdgt].set(self.text)
-			self.obj[self.wdgt].show()		
-		elif self.cur_step == 1:
-			self.obj[self.wdgt].hide()
-			self.is_finished = True
-		self.cur_step += 1
-		
-	def set_flash(self,ar,clr,dly):
-		self.ar = ar
-		self.clr = clr
-		self.delay_ms = dly
-		self.action = self.flash
-		self.init = dummy
+    def init_move(self):
+        self.strt = {k: (v.x, v.y) for k, v in self.obj.items()}
+        self.delta = {}
+        for k, v in self.tgt.items():
+            dx = (self.tgt[k][0] - self.strt[k][0]) * 1.0 / self.steps
+            dy = (self.tgt[k][1] - self.strt[k][1]) * 1.0 / self.steps
+            self.delta[k] = (dx, dy)
+        self.action = self.move
 
-	def flash(self):
-		if self.cur_step in (0,2):
-			for w in self.ar:
-				self.obj[w].bg(self.clr)
-		else:
-			for w in self.ar:
-				self.obj[w].reset_bg()
-		if self.cur_step >2:
-			self.is_finished = True
-		self.cur_step += 1
-		
-	def set_change_text(self,ar,dly):
-		self.ar = ar
-		self.delay_ms = dly
-		self.init = dummy
-		self.action = self.change_text
+    def move(self):
+        for k in self.tgt.keys():
+            obj = self.obj[k]
+            dx, dy = self.delta[k]
+            obj.move(obj.x + dx, obj.y + dy)
+        self.cur_step += 1
+        if self.cur_step >= self.steps:
+            self.is_finished = True
 
-	def change_text(self):
-		w,v = self.ar[self.cur_step]
-		self.obj[w].set(v)
-		if self.cur_step >= len(self.ar) -1:
-			self.is_finished = True
-		self.cur_step += 1
-		
+    def set_show_once(self, wdgt, text, dly):
+        self.wdgt = wdgt
+        self.text = text
+        self.delay_ms = dly
+        self.init = dummy
+        self.action = self.show_once
 
+    def show_once(self):
+        if self.cur_step == 0:
+            self.obj[self.wdgt].set(self.text)
+            self.obj[self.wdgt].show()
+        elif self.cur_step == 1:
+            self.obj[self.wdgt].hide()
+            self.is_finished = True
+        self.cur_step += 1
 
-	def next_step(self):
-		self.action()
+    def set_flash(self, ar, clr, dly):
+        self.ar = ar
+        self.clr = clr
+        self.delay_ms = dly
+        self.action = self.flash
+        self.init = dummy
+
+    def flash(self):
+        if self.cur_step in (0, 2):
+            for w in self.ar:
+                self.obj[w].bg(self.clr)
+        else:
+            for w in self.ar:
+                self.obj[w].reset_bg()
+        if self.cur_step > 2:
+            self.is_finished = True
+        self.cur_step += 1
+
+    def set_change_text(self, ar, dly):
+        self.ar = ar
+        self.delay_ms = dly
+        self.init = dummy
+        self.action = self.change_text
+
+    def change_text(self):
+        w, v = self.ar[self.cur_step]
+        self.obj[w].set(v)
+        if self.cur_step >= len(self.ar) - 1:
+            self.is_finished = True
+        self.cur_step += 1
+
+    def next_step(self):
+        self.action()
--- friendshiip_day/main.py
+++ friendshiip_day/main.py
@@ -5,92 +5,163 @@ obj = {}
 scn = []
 scn_id = 0
 
+
 def load_window():
-	fr = app.add_frame('Enter Details',width=880,height=630,xy=[10,10])
-	obj['lbl1'] = fr.add_label('Name',width=12,height=1,xy=[50,50])
-	obj['txt1'] = fr.add_text('John Smith',width=10,height=1,xy=[400,50])
-	obj['lbl2'] = fr.add_label('Gender',width=12,height=1,xy=[50,150])
-	obj['dd1'] = fr.add_dropdown(['Male','Female'],'Male',xy=[400,130])
-	obj['lbl3'] = fr.add_label('Age',width=12,height=1,xy=[50,250])
-	obj['txt2'] = fr.add_text('57',width=10,height=1,xy=[400,250])
-	obj['lbl4'] = fr.add_label('City',width=12,height=1,xy=[50,350])
-	obj['txt3'] = fr.add_text('Chennai',width=10,height=1,xy=[400,350])
-	obj['btn1'] = fr.add_button('Submit',cb=None,xy=[300,450])
-	obj['lblovr'] = fr.add_label('To all the text boxes',width=32,height=3,xy=[10,150])
-	obj['lblovr'].hide()	
-	
+    fr = app.add_frame('Enter Details', width=880, height=630, xy=[10, 10])
+    obj['lbl1'] = fr.add_label('Name', width=12, height=1, xy=[50, 50])
+    obj['txt1'] = fr.add_text('John Smith', width=10, height=1, xy=[400, 50])
+    obj['lbl2'] = fr.add_label('Gender', width=12, height=1, xy=[50, 150])
+    obj['dd1'] = fr.add_dropdown(['Male', 'Female'], 'Male', xy=[400, 140])
+    obj['lbl3'] = fr.add_label('Age', width=12, height=1, xy=[50, 250])
+    obj['txt2'] = fr.add_text('57', width=10, height=1, xy=[400, 250])
+    obj['lbl4'] = fr.add_label('City', width=12, height=1, xy=[50, 350])
+    obj['txt3'] = fr.add_text('Chennai', width=10, height=1, xy=[400, 350])
+    obj['btn1'] = fr.add_button('Submit', cb=None, xy=[300, 450])
+    obj['lblovr'] = fr.add_label('To all the text boxes', width=32, height=3, xy=[10, 150])
+    obj['lblovr'].hide()
+
 
 def load_scenes():
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_show_once('lblovr','To all the Text Boxes', 2000)
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_flash('txt1 txt2 txt3'.split(),'#368BC1',200)
-	
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_show_once('lblovr','And to all the Drop Downs', 2000)
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_flash(['dd1'],'#FBB917',200)
-	
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_show_once('lblovr','And to all the Labels', 2000)
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_flash('lbl1 lbl2 lbl3 lbl4'.split(),'#B87333',200)
-	
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_show_once('lblovr','And to all the Buttons', 2000)
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_flash(['btn1'],'#FF8040',200)
+    scn.append(Scene(obj, 2000))
+    scn[-1].set_show_once('lblovr', 'To all the Text Boxes', 2000)
+    scn.append(Scene(obj, 2000))
+    scn[-1].set_flash('txt1 txt2 txt3'.split(), '#368BC1', 200)
 
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_show_once('lblovr','Take a break', 2000)
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_show_once('lblovr','And raise a toast', 2000)
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_show_once('lblovr','To my best friend ...', 2000)
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_show_once('lblovr','Mukesh !!!', 2000)
-	
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_move({
-	'lbl4': (100,50)
-	,'dd1': (450,30)
-	,'lbl1': (50,150)
-	,'lbl2': (300,150)
-	,'lbl3': (550,150)
-	,'txt1': (50,250)
-	,'txt2': (300,250)
-	,'txt3': (550,250)
-	,'btn1': (300,350)})
-	scn.append( Scene(obj,2000) )
-	scn[-1].set_change_text(
-	(('lbl4','***')
-	,('dd1','***')
-	,('lbl1','Wish')
-	,('lbl2','You')
-	,('lbl3','A')
-	,('txt1','Happy')
-	,('txt2','Friendship')
-	,('txt3','Day')
-	,('btn1','Mukesh !!!')),400)
-	
-	scn[0].init()
-	
+    scn.append(Scene(obj, 2000))
+    scn[-1].set_show_once('lblovr', 'And to all the Drop Downs', 2000)
+    scn.append(Scene(obj, 2000))
+    scn[-1].set_flash(['dd1'], '#FBB917', 200)
+
+    scn.append(Scene(obj, 2000))
+    scn[-1].set_show_once('lblovr', 'And to all the Labels', 2000)
+    scn.append(Scene(obj, 2000))
+    scn[-1].set_flash('lbl1 lbl2 lbl3 lbl4'.split(), '#B87333', 200)
+
+    scn.append(Scene(obj, 2000))
+    scn[-1].set_show_once('lblovr', 'And to all the Buttons', 2000)
+    scn.append(Scene(obj, 2000))
+    scn[-1].set_flash(['btn1'], '#FF8040', 200)
+
+    scn.append(Scene(obj, 1))
+    scn[-1].set_show_once('lblovr', 'Take a break', 2000)
+    scn.append(Scene(obj, 1))
+    scn[-1].set_show_once('lblovr', 'And raise a toast', 2000)
+    scn.append(Scene(obj, 1))
+    scn[-1].set_show_once('lblovr', 'For my best friend ...', 2000)
+    scn.append(Scene(obj, 4000))
+    scn[-1].set_show_once('lblovr', 'Mukesh !!!', 2000)
+
+    scn.append(Scene(obj, 2000))
+    scn[-1].set_move({
+        'lbl4': (100, 50)
+        , 'dd1': (450, 40)
+        , 'lbl1': (50, 150)
+        , 'lbl2': (300, 150)
+        , 'lbl3': (550, 150)
+        , 'txt1': (50, 250)
+        , 'txt2': (300, 250)
+        , 'txt3': (550, 250)
+        , 'btn1': (300, 350)})
+    scn.append(Scene(obj, 4000))
+    scn[-1].set_change_text(
+        (('lbl4', '***')
+         , ('dd1', '***')
+         , ('lbl1', 'Wish')
+         , ('lbl2', 'You')
+         , ('lbl3', 'A')
+         , ('txt1', 'Happy')
+         , ('txt2', 'Friendship')
+         , ('txt3', 'Day')
+         , ('btn1', 'Mukesh !!!')), 400)
+
+    scn.append(Scene(obj, 2000))
+    scn[-1].set_move({
+        'lbl1': (50, 150)
+        , 'lbl2': (300, 150)
+        , 'lbl3': (550, 150)
+        , 'txt1': (50, 250)
+        , 'txt2': (300, 250)
+        , 'txt3': (550, 250)
+        ,'lbl4': (50, 350)
+        , 'btn1': (300, 330)
+        , 'dd1': (550, 340)})
+    scn.append(Scene(obj, 4000))
+    scn[-1].set_change_text(
+        (('lbl1','I')
+        ,('lbl2', 'am')
+        ,('lbl3', 'blessed')
+        ,('txt1', 'to')
+        ,('txt2', 'have' )
+        ,('txt3','a')
+        ,('lbl4','friend')
+        ,('btn1','like')
+        ,('dd1','you')), 400)
+
+    scn.append(Scene(obj, 2000))
+    scn[-1].set_move({
+        'dd1': (300, 40)
+        , 'txt1': (50, 150)
+        , 'txt2': (300, 150)
+        , 'txt3': (550, 150)
+        , 'btn1': (300, 230)
+        , 'lbl1': (100, 350)
+        , 'lbl2': (400, 350)
+        , 'lbl3': (100, 450)
+        , 'lbl4': (400, 450)})
+    scn.append(Scene(obj, 4000))
+    scn[-1].set_change_text(
+        (('dd1', '***')
+         , ('txt1', 'May')
+         , ('txt2', 'all')
+         , ('txt3', 'your')
+         , ('btn1', 'dreams')
+         , ('lbl1', 'come')
+         , ('lbl2', 'true')
+         , ('lbl3', '***')
+         , ('lbl4', '***')), 400)
+
+    scn.append(Scene(obj, 2000))
+    scn[-1].set_move({
+        'lbl1': (50, 50)
+        , 'txt1': (400, 50)
+        , 'lbl2': (50, 150)
+        , 'dd1': (400, 140)
+        , 'lbl3': (50, 250)
+        , 'txt2': (400, 250)
+        , 'lbl4': (50, 350)
+        , 'txt3': (400, 350)
+        , 'btn1': (300, 450)})
+    scn.append(Scene(obj, 4000))
+    scn[-1].set_change_text(
+        (('lbl1', 'Name')
+         , ('txt1', 'John Smith')
+         , ('lbl2', 'Gender')
+         , ('dd1', 'Male')
+         , ('lbl3', 'Age')
+         , ('txt2', '57')
+         , ('lbl4', 'City')
+         , ('txt3', 'Chennai')
+         , ('btn1', 'Submit')), 400)
+
+    scn[0].init()
+
 
 def animate():
-	global scn_id
-	s = scn[scn_id]
-	s.next_step()
-	if not s.is_finished:
-		app.top.after(s.delay_ms,animate)
-	elif scn_id < len(scn) - 1:
-		dly = s.pafter
-		scn_id += 1		
-		s = scn[scn_id]
-		s.init()
-		app.top.after(dly,animate)
-				
-app = MyApp('Happy Friendship Day !!',width=900,height=650)
+    global scn_id
+    s = scn[scn_id]
+    s.next_step()
+    if not s.is_finished:
+        app.top.after(s.delay_ms, animate)
+    elif scn_id < len(scn) - 1:
+        dly = s.pafter
+        scn_id += 1
+        s = scn[scn_id]
+        s.init()
+        app.top.after(dly, animate)
+
+
+app = MyApp('Happy Friendship Day !!', width=900, height=650)
 load_window()
 load_scenes()
-app.top.after(3000,animate)
+app.top.after(3000, animate)
 app.show()
