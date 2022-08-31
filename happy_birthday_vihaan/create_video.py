7595f1983d503f903b39420056d3edcfc1fdec01
Dhaval Shah <seedhaval@gmail.com>
2022-08-31 16:23:49 +0530

    happy birthday letter by letter

--- happy_birthday_vihaan/create_video.py
+++ happy_birthday_vihaan/create_video.py
@@ -1,19 +1,23 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 import glob
 import random
 from moviepy.editor import *
 
 car_lst = list(glob.glob('car_img/*.*'))
+cnv_width = 1600
+cnv_height = 900
+img_px = 150
 
-l,r,u,d = "left right up down".split()
+l, r, u, d = "left right up down".split()
+
 
 class Car:
-    def __init__(self,ix,iy,dr,static):
-        self.left = 80 + (ix*40)
-        self.top = 70 + (iy*40)
-        self.ix = ix
-        self.iy = iy
+    def __init__(self, ix, iy, dr, static, lftdx, topdx):
+        self.left = ix
+        self.top = iy
         self.dr = dr
+        self.lftdx = lftdx
+        self.topdx = topdx
         self.fl = random.choice(car_lst)
         self.img = Image.open(self.fl)
         if dr == d:
@@ -23,12 +27,20 @@ class Car:
             self.img = self.img.transpose(Image.FLIP_TOP_BOTTOM)
         elif dr == u:
             self.img = self.img.transpose(Image.ROTATE_270)
-        self.start = random.choice((5,40))
-        self.speed = random.choice((8,18))
+        self.base_img = self.img.copy()
+        self.speed = random.choice((15, 38))
         self.static = static
 
     def shift(self, fid):
-        if self.static or fid < self.start:
+        if fid < 5:
+            return
+        elif self.static and 25 < fid < 55:
+            sz = int(img_px - ((img_px - 40) / 30) * (fid - 25))
+            self.left -= self.lftdx
+            self.top -= self.topdx
+            self.img = self.base_img.resize((sz, sz))
+            self.img = ImageEnhance.Brightness(self.img).enhance(1+((fid-25)*.1))
+        elif self.static:
             return
         elif self.dr == d:
             self.top += self.speed
@@ -39,35 +51,87 @@ class Car:
         elif self.dr == r:
             self.left += self.speed
 
-def draw_img(car_ar,fid):
-    bg = Image.open('base.jpg')
-    for car in car_ar:
-        car.shift(fid)
-        bg.paste(car.img,(car.left,car.top),car.img)
+
+class Letter:
+    def __init__(self, irow, icol, strtpos, width, ltr):
+        self.irow = irow
+        self.icol = icol
+        self.ltr = ltr
+        self.strtpos = strtpos
+        self.width = width
+        print(self.width)
+        self.car_ar = []
+        self.load_cars()
+
+    def load_cars(self):
+        for ir, row in enumerate(self.ltr):
+            for ic, cell in enumerate(row):
+                if cell == "|":
+                    dr = random.choice((u, d))
+                    static = True
+                elif cell == "-":
+                    dr = random.choice((l, r))
+                    static = True
+                else:
+                    dr = random.choice((l, r, u, d))
+                    static = False
+                left = (cnv_width - (self.width * img_px)) // 2 + (ic * img_px)
+                top = (cnv_height - (5 * img_px)) // 2 + (ir * img_px)
+                tgtlft = 50 + (strtpos + ic) * 40
+                tgttop = 50 + (250 * self.irow) + (ir * 40)
+                lftdx = (left - tgtlft) / 30
+                topdx = (top - tgttop) / 30
+                self.car_ar.append(Car(left, top, dr, static, lftdx, topdx))
+
+    def shift(self, fid):
+        for car in self.car_ar:
+            car.shift(fid)
+
+    def add_img(self, bg):
+        for car in self.car_ar:
+            bg.paste(car.img, (int(car.left), int(car.top)), car.img)
+
+
+def draw_img(ltr_ar, fid):
+    bg = Image.new('RGB', (1600, 900), (255, 255, 255))
+    for i, ltr in enumerate(ltr_ar):
+        ltr_strt = (i * 45)
+        if ltr_strt < fid:
+            ltr.shift(fid - ltr_strt)
+            ltr.add_img(bg)
     bg.save('out_frames/out_%03d.png' % fid)
 
+
 def get_text_map():
+    ar = []
+    i = 1
     with open('text_map.csv') as f:
-        return [x.strip().split(',') for x in f.readlines() if x.strip()]
+        for ln in [x.strip().rstrip(',') for x in f.readlines() if x.strip().strip(',')]:
+            pts = [[x.split(',')] for x in (ln + ',').split(',x,')[:-1]]
+            if i % 5 == 1:
+                curpts = pts
+            else:
+                for j, v in enumerate(pts):
+                    curpts[j].extend(v)
+            if i % 5 == 0:
+                i = 0
+                ar.append(curpts)
+            i += 1
+    return ar
 
+
+ar = []
 txt_map = get_text_map()
-car_ar = []
-for ir, row in enumerate(txt_map):
-    for ic, cell in enumerate(row):
-        if cell == "|":
-            dr = random.choice((u,d))
-            static = True
-        elif cell == "-":
-            dr = random.choice((l,r))
-            static = True
-        else:
-            dr = random.choice((l,r,u,d))
-            static = False
-        car_ar.append(Car(ic,ir,dr,static))
+for irow, ln in enumerate(txt_map):
+    strtpos = 0
+    for icol, ltr in enumerate(ln):
+        width = len(ltr[0])
+        ar.append(Letter(irow, icol, strtpos, width, ltr))
+        strtpos += width + 1
 
-for fid in range(300):
+for fid in range(900):
     print(fid)
-    draw_img(car_ar,fid)
+    draw_img(ar, fid)
 
-clip = ImageSequenceClip(sorted(glob.glob('out_frames/*.*')),fps=10)
+clip = ImageSequenceClip(sorted(glob.glob('out_frames/*.*')), fps=20)
 clip.write_videofile('out.mp4')
