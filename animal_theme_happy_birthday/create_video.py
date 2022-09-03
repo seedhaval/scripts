0d7c2b560d710f6e2417b70ba3d81f40d3fa10fb
Dhaval Shah <seedhaval@gmail.com>
2022-09-03 21:09:25 +0530

    animal theme happy birthday

--- animal_theme_happy_birthday/create_video.py
+++ animal_theme_happy_birthday/create_video.py
@@ -39,19 +39,20 @@ class Wheel:
         x1, y1 = rotate_point(-self.inner_radius, 0, self.angle, self.image_length / 2, self.image_length / 2)
         x2, y2 = rotate_point(self.inner_radius, 0, self.angle, self.image_length / 2, self.image_length / 2)
         mask_draw.line((x1, y1, x2, y2), fill=255, width=self.spoke_width)
-        x1, y1 = rotate_point(-self.inner_radius, 0, self.angle+90, self.image_length / 2, self.image_length / 2)
-        x2, y2 = rotate_point(self.inner_radius, 0, self.angle+90, self.image_length / 2, self.image_length / 2)
+        x1, y1 = rotate_point(-self.inner_radius, 0, self.angle + 90, self.image_length / 2, self.image_length / 2)
+        x2, y2 = rotate_point(self.inner_radius, 0, self.angle + 90, self.image_length / 2, self.image_length / 2)
         mask_draw.line((x1, y1, x2, y2), fill=255, width=self.spoke_width)
         self.img.putalpha(mask_img)
 
     def shift(self):
-        self.angle = ( self.angle - self.speed ) % 360
+        self.angle = (self.angle - self.speed) % 360
         self.recreate_image()
 
+
 class Engine:
-    def __init__(self,left,top):
-        self.width=470
-        self.height=600
+    def __init__(self, left, top):
+        self.width = 470
+        self.height = 600
         self.engine = Image.open('engine.png')
         self.engine_height, self.engine_width = self.engine.size
         self.whl1 = Wheel()
@@ -63,10 +64,10 @@ class Engine:
         self.recreate_image()
 
     def recreate_image(self):
-        self.img = Image.new('RGBA',(self.width,self.height))
-        self.img.paste(self.engine,(0,self.height - (self.engine_height - 45)),self.engine)
-        self.img.paste(self.whl1.img,(50,self.height-110),self.whl1.img)
-        self.img.paste(self.whl2.img,(self.engine_width-60,self.height-110),self.whl2.img)
+        self.img = Image.new('RGBA', (self.width, self.height))
+        self.img.paste(self.engine, (0, self.height - (self.engine_height - 45)), self.engine)
+        self.img.paste(self.whl1.img, (50, self.height - 110), self.whl1.img)
+        self.img.paste(self.whl2.img, (self.engine_width - 60, self.height - 110), self.whl2.img)
 
     def shift(self):
         self.left += self.speed
@@ -74,12 +75,13 @@ class Engine:
         self.whl2.shift()
         self.recreate_image()
 
+
 class Trolley:
-    def __init__(self,left,top,color,animal):
-        self.width=800
-        self.height=600
-        self.trolley_height=150
-        self.trolley_width=700
+    def __init__(self, left, top, color, animal):
+        self.width = 800
+        self.height = 600
+        self.trolley_height = 150
+        self.trolley_width = 700
         self.connector_width = self.width - self.trolley_width
         self.trolley_color = color
         self.whl1 = Wheel()
@@ -90,21 +92,22 @@ class Trolley:
         self.img = None
         self.animal_width = 400
         self.animal = Image.open('animals/%s.png' % animal)
-        w,h = self.animal.size
-        factor = 400/w
-        self.animal = self.animal.resize((int(w*factor),int(h*factor)))
-        w,h = self.animal.size
+        w, h = self.animal.size
+        factor = 400 / w
+        self.animal = self.animal.resize((int(w * factor), int(h * factor)))
+        w, h = self.animal.size
         self.animal_top = self.height - (self.trolley_height + 5 + h)
         self.recreate_image()
 
     def recreate_image(self):
-        self.img = Image.new('RGBA',(self.width,self.height))
-        self.img.paste(self.animal,(0,self.animal_top),self.animal)
+        self.img = Image.new('RGBA', (self.width, self.height))
+        self.img.paste(self.animal, (0, self.animal_top), self.animal)
         draw = ImageDraw.Draw(self.img)
-        draw.rectangle((0,self.height-self.trolley_height-55,self.trolley_width,self.height-55),fill=self.trolley_color)
-        self.img.paste(self.whl1.img,(50,self.height-110),self.whl1.img)
-        self.img.paste(self.whl2.img,(self.trolley_width-160,self.height-110),self.whl2.img)
-        draw.rectangle((self.trolley_width,self.height-90,self.width,self.height-70),fill=(0,0,0))
+        draw.rectangle((0, self.height - self.trolley_height - 55, self.trolley_width, self.height - 55),
+                       fill=self.trolley_color)
+        self.img.paste(self.whl1.img, (50, self.height - 110), self.whl1.img)
+        self.img.paste(self.whl2.img, (self.trolley_width - 160, self.height - 110), self.whl2.img)
+        draw.rectangle((self.trolley_width, self.height - 90, self.width, self.height - 70), fill=(0, 0, 0))
 
     def shift(self):
         self.left += self.speed
@@ -112,24 +115,84 @@ class Trolley:
         self.whl2.shift()
         self.recreate_image()
 
-t_ar = []
-clr_ar = [(255,140,0),(102,205,170),(123,104,238),(255,105,180),(176,196,222)]
-animal_ar = ('elephant','duck','giraffe','penguin','panda')
-engine=Engine(-470,250)
-left = -470 - 800
-for i in range(5):
-    t_ar.append(Trolley(left,250,clr_ar[i],animal_ar[i]))
-    left -= 800
 
-for i in range(500):
-    print('processing frame ',i)
-    bg = Image.open('base_img.jpg')
-    engine.shift()
+class Gift:
+    def __init__(self, color, gift, left, top):
+        self.color = color
+        self.left = left
+        self.top = top
+        self.gift = Image.open('gifts/%s.png' % gift)
+        self.width = 400
+        self.height = 600
+        self.box_height = 150
+        self.box_width = 200
+        self.box_top = self.height - self.box_height
+        self.angle = 0
+        self.fid = 0
+        self.img = None
+        self.speed = 5
+        self.mode = 'move up'
+
+    def recreate_image(self):
+        self.img = Image.new('RGBA',(self.width,self.height))
+        draw = ImageDraw.Draw(self.img)
+        draw.rectangle((0,self.box_top,self.box_width,self.box_top+self.box_height),fill=self.color)
+        x1, y1 = rotate_point(-200,0,self.angle,200,self.box_top)
+        x2, y2 = (200,self.box_top)
+        draw.line((x1,y1,x2,y2),fill=self.color,width=30)
+
+    def shift(self):
+        if self.mode == 'move up':
+            self.box_top -= self.speed
+        elif self.mode == 'open box':
+            self.angle -= self.speed
+        self.recreate_image()
+        self.fid += 1
+        if self.mode == 'move up' and self.fid > 20:
+            self.mode = 'open box'
+        elif self.mode == 'open box' and self.fid > 40:
+            self.mode = 'pop out'
+
+def draw_train(bg,shift=False):
+    if shift == True:
+        engine.shift()
     bg.paste(engine.img, (engine.left, engine.top), engine.img)
     for t in t_ar:
-        t.shift()
+        if shift == True:
+            t.shift()
         bg.paste(t.img, (t.left, t.top), t.img)
+
+t_ar = []
+gift_ar = []
+clr_ar = [(255, 140, 0), (102, 205, 170), (123, 104, 238), (255, 105, 180), (176, 196, 222)]
+animal_ar = ('elephant', 'duck', 'giraffe', 'penguin', 'panda')
+gft_lst = ('teddy_bear','barbie','chocolate','cake','ball')
+engine = Engine(-470, 250)
+left = -470 - 800
+for i in range(5):
+    t_ar.append(Trolley(left, 250, clr_ar[i], animal_ar[i]))
+    gift_ar.append(Gift(clr_ar[i],gft_lst[i],725,195))
+    left -= 800
+
+i = 0
+cur_idx = 0
+for j in range(600):
+    print('processing frame ', i)
+    if t_ar[cur_idx].left > 325:
+        g = gift_ar[cur_idx]
+        for k in range(40):
+            print('processing frame ', i)
+            g.shift()
+            bg = Image.open('base_img.jpg')
+            bg.paste(g.img, (g.left,g.top),g.img)
+            draw_train(bg,False)
+            bg.save('out_frames/img_%03d.png' % i)
+            i += 1
+        cur_idx += 1
+    bg = Image.open('base_img.jpg')
+    draw_train(bg,True)
     bg.save('out_frames/img_%03d.png' % i)
+    i += 1
 
 clip = ImageSequenceClip(sorted(glob.glob('out_frames/*.*')), fps=20)
 clip.write_videofile('out.mp4')
