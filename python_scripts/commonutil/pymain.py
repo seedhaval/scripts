3fbe5099e0f00c58b22943b5cd8452a6a182874f
Dhaval Shah <seedhaval@gmail.com>
2022-09-25 11:15:25 +0530

    creating ppt

--- python_scripts/commonutil/pymain.py
+++ python_scripts/commonutil/pymain.py
@@ -2,9 +2,12 @@ import sys
 sys.path.append(r"D:\scripts\scripts")
 
 from python_scripts.commonutil.helper import get_choice_from_user
-from python_scripts.youtube_video_utilities import create_new_project
+from python_scripts.youtube_video_utilities import create_new_project, create_walkthrough_ppt
 
-ch = get_choice_from_user("Select action",["Create new Youtube project"])
+ch = get_choice_from_user("Select action",["Create new Youtube project",
+                                           "Create walkthrough PPT"])
 
 if ch == "Create new Youtube project":
     create_new_project.main()
+elif ch == "Create walkthrough PPT":
+    create_walkthrough_ppt.main()
--- python_scripts/youtube_video_utilities/create_walkthrough_ppt.py
+++ python_scripts/youtube_video_utilities/create_walkthrough_ppt.py
@@ -1,38 +1,55 @@
 from python_scripts.commonutil.helper import File
 from pygments import highlight
-from pygments.lexers import PythonLexer
 from pygments.formatters import ImageFormatter
 from pygments import lexers
+from pptx import Presentation
+from pptx.util import Inches
 
-height = 31
-max_scroll = 20
-buffer = 5
-width = 81
 
-fl = File(r"D:\scripts\scripts\python_scripts\commonutil\tkhelper.py")
-data = fl.readlines()
-data.extend([' ']*10)
-data_fmt = [x+' '*(width-len(x))+'.' for x in data]
-cnt = len(data)
-img_fldr = r"D:\tmp"
-start = 0
+def main():
+    height = 31
+    max_scroll = 20
+    buffer = 5
+    width = 81
 
-lexer = lexers.get_lexer_by_name("python", stripnl=False)
+    fl = File(input("Enter full path to Python script : "))
+    data = fl.readlines()
+    data.extend([' '] * 10)
+    data_fmt = [x + ' ' * (width - len(x)) + '.' for x in data]
+    cnt = len(data)
+    img_fldr = r"D:\tmp"
+    start = 0
+    ppt_fl = r"C:\Users\Dell\OneDrive\Desktop\code_walkthrough.pptx"
+    tmplt_fl = r"C:\Users\Dell\OneDrive\Documents\template\code_walkthrough" \
+               r".pptx"
 
-for i, ln in enumerate(data, 1):
-    ln_strp = ln.strip()
-    ln = ln + (' ' * (width - len(ln)))
-    if ln_strp.startswith('import ') or ln_strp.startswith('from '):
-        pass
-    elif ln_strp == '':
-        pass
-    else:
-        if i - start >= max_scroll:
-            start = i - 1 - buffer
-            if start + height > cnt:
-                start = cnt - height
-        code = '\n'.join(data_fmt[start:start + height])
-        with open(img_fldr + "\\" + ("%03d" % i) + ".png", "wb") as f:
-            f.write(highlight(code, lexer,
-                              ImageFormatter(line_number_chars=3, line_number_start=start + 1, font_size=28,
-                                             hl_lines=[i - start])))
+    lexer = lexers.get_lexer_by_name("python", stripnl=False)
+
+    prs = Presentation(tmplt_fl)
+    title_slide_layout = prs.slide_layouts[0]
+
+    for i, ln in enumerate(data, 1):
+        ln_strp = ln.strip()
+        ln = ln + (' ' * (width - len(ln)))
+        if ln_strp.startswith('import ') or ln_strp.startswith('from '):
+            pass
+        elif ln_strp == '':
+            pass
+        else:
+            if i - start >= max_scroll:
+                start = i - 1 - buffer
+                if start + height > cnt:
+                    start = cnt - height
+            code = '\n'.join(data_fmt[start:start + height])
+            img_fl = img_fldr + "\\" + ("%03d" % i) + ".png"
+            with open(img_fl, "wb") as f:
+                f.write(highlight(code, lexer,
+                                  ImageFormatter(line_number_chars=3,
+                                                 line_number_start=start + 1,
+                                                 font_size=28,
+                                                 hl_lines=[i - start])))
+            slide = prs.slides.add_slide(title_slide_layout)
+            slide.shapes.add_picture(img_fl, left=0, top=0, width=Inches(13.4),
+                                     height=Inches(7.5))
+
+    prs.save(ppt_fl)
