a7f52c625e01e0d1c6fa7958d2c3b2b5db1d721f
Dhaval Shah <seedhaval@gmail.com>
2022-10-02 22:20:54 +0530

    creating anim json

--- python_scripts/youtube_video_utilities/generate_video.py
+++ python_scripts/youtube_video_utilities/generate_video.py
@@ -1,13 +1,29 @@
 import subprocess
 import os
 from pathlib import Path
+import re
+import shutil
 
 def generate(fl):
-    subprocess.check_output( ['ffmpeg', '-i', fl+'_noaudio.mp4', '-i',
+    prcs = subprocess.Popen( ['ffmpeg', '-i', fl+'_noaudio.mp4', '-i',
                                fl+'.wav', '-map', '0:v', '-map', '1:a',
                               '-c:v', 'copy', '-shortest', fl+'.mp4'] )
+    prcs.wait()
+    if prcs.returncode != 0:
+        return prcs.returncode
+    base_nm = re.sub(r'_\d+$', '', fl)
+    cmplt_fldr = "completed/" + base_nm
+    shutil.move(fl+".wav", cmplt_fldr + "/" + fl + ".wav")
+    shutil.move(fl + "_noaudio.mp4", cmplt_fldr + "/" + fl + "_noaudio.mp4")
+    shutil.copy(fl + ".mp4", cmplt_fldr + "/" + fl + ".mp4")
+    os.remove(fl + ".aup3")
 
 def main():
     os.chdir(r'D:\Documents\notes_doodle')
     for fl in Path('.').glob('*.wav'):
-        generate(fl.stem)
+        rc = generate(fl.stem)
+        if rc != 0:
+            return
+
+if __name__ == "__main__":
+    main()
