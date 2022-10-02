be4743ced29d9ea1869b85081315c7758ea9c270
Dhaval Shah <seedhaval@gmail.com>
2022-10-02 20:34:09 +0530

    creating anim json

--- python_scripts/commonutil/pymain.py
+++ python_scripts/commonutil/pymain.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append(r"D:\scripts\scripts")
 
 from python_scripts.commonutil.helper import get_choice_from_user
@@ -6,22 +7,24 @@ from python_scripts.youtube_video_utilities import create_new_project, \
     create_walkthrough_ppt, generate_video, generate_benime_file, \
     create_audacity_files, add_noaudio_file_name
 
-ch = get_choice_from_user("Select action",["Create new Youtube project",
-                                           "Create walkthrough PPT",
-                                           "Generate Benime file",
-                                           "Generate video",
-                                           "Create audacity file",
-                                           "Add _noaudio to file name"])
+ch_ar = ["Create new Youtube project",
+         "Create walkthrough PPT",
+         "Generate Benime file",
+         "Generate video",
+         "Create audacity file",
+         "Add _noaudio to file name"]
 
-if ch == "Create new Youtube project":
-    create_new_project.main()
-elif ch == "Create walkthrough PPT":
-    create_walkthrough_ppt.main()
-elif ch == "Generate video":
-    generate_video.main()
-elif ch == "Generate Benime file":
-    generate_benime_file.main()
-elif ch == "Create audacity file":
-    create_audacity_files.main()
-elif ch == "Add _noaudio to file name":
-    add_noaudio_file_name.main()
+while True:
+    ch = get_choice_from_user("Select action", ch_ar)
+    if ch == "Create new Youtube project":
+        create_new_project.main()
+    elif ch == "Create walkthrough PPT":
+        create_walkthrough_ppt.main()
+    elif ch == "Generate video":
+        generate_video.main()
+    elif ch == "Generate Benime file":
+        generate_benime_file.main()
+    elif ch == "Create audacity file":
+        create_audacity_files.main()
+    elif ch == "Add _noaudio to file name":
+        add_noaudio_file_name.main()
