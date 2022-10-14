d332ec0b5df365d6b7f910bd358d1233c79f0de3
seedhaval <seedhaval@gmail.com>
2022-10-14 23:50:17 +0530

    Update env_setup.sh

--- shell_scripts/userland/env_setup.sh
+++ shell_scripts/userland/env_setup.sh
@@ -14,15 +14,12 @@ read -p "Enter user name : " usr
 read -p "Enter email : " eml
 git config --global user.name "${usr}"
 git config --global user.email "${eml}"
-cat ~/.ssh/id_rsa.pub
-echo ""
-echo "Copy above ssh key to github and then press enter"
-read a
+
 
 
 echo "Cloning repository"
 cd
-git clone git@github.com:seedhaval/scripts.git
+git clone https://seedhaval@github.com/seedhaval/scripts.git
 
 echo "Transfering control to env_setup.py to carry out checkpointed setup"
 python3 ~/scripts/shell_scripts/userland/env_setup.py
