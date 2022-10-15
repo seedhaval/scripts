8f8e4dc7d158053e5bfa8d0acfb58dbe95528c93
Dhaval Shah <seedhaval@gmail.com>
2022-10-15 18:30:27 +0530

    Rename file

--- python_scripts/youtube_video_utilities/generate_video.py
+++ python_scripts/youtube_video_utilities/generate_video.py
@@ -4,20 +4,26 @@ from pathlib import Path
 import re
 import shutil
 
+
 def generate(fl):
-    prcs = subprocess.Popen( ['ffmpeg', '-i', fl+'_noaudio.mp4', '-i',
-                               fl+'.wav', '-map', '0:v', '-map', '1:a',
-                              '-c:v', 'copy', '-shortest', fl+'.mp4'] )
+    prcs = subprocess.Popen(['ffmpeg', '-i', fl + '_noaudio.mp4', '-i',
+                             fl + '.wav', '-map', '0:v', '-map', '1:a',
+                             '-c:v', 'copy', '-shortest', fl + '.mp4'])
     prcs.wait()
     if prcs.returncode != 0:
         return prcs.returncode
     base_nm = re.sub(r'_\d+$', '', fl)
+    part = re.findall(r'\d+$', fl)[0]
     cmplt_fldr = "completed/" + base_nm
-    shutil.move(fl+".wav", cmplt_fldr + "/" + fl + ".wav")
+    with open(cmplt_fldr + "/" + fl + ".txt") as f:
+        title = f.readlines()[0].strip()
+    shutil.move(fl + ".wav", cmplt_fldr + "/" + fl + ".wav")
     shutil.move(fl + "_noaudio.mp4", cmplt_fldr + "/" + fl + "_noaudio.mp4")
     shutil.copy(fl + ".mp4", cmplt_fldr + "/" + fl + ".mp4")
     os.remove(fl + ".aup3")
+    shutil.move(fl + ".mp4", "Revision of " + title + " - " + part + ".mp4")
     return 0
+
 
 def main():
     os.chdir(r'D:\Documents\notes_doodle')
@@ -25,6 +31,7 @@ def main():
         rc = generate(fl.stem)
         if rc != 0:
             return
+
 
 if __name__ == "__main__":
     main()
