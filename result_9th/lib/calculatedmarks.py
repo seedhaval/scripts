7a8a2c0242e035ea526496b65ff0f6f750ede6d2
Dhaval Shah <seedhaval@gmail.com>
2023-02-26 19:37:15 +0530

    sanskrit, hindi, english

--- result_9th/lib/calculatedmarks.py
+++ result_9th/lib/calculatedmarks.py
@@ -53,3 +53,44 @@ def calculate(md, type, cols):
         md["mat.15"] = md["mat.7"] + md["mat.14"]
     if (type == 'all' or 'mat.16' in cols) and isvalid("mat.15", md):
         md["mat.16"] = md["mat.15"] / 2
+
+    if (type == 'all' or 'snsk.1' in cols) and isvalid("15 16 17 18", md):
+        md["snsk.1"] = md["15"] + md["16"] + md["17"] + md["18"]
+    if (type == 'all' or 'snsk.2' in cols) and isvalid("14 snsk.1", md):
+        md["snsk.2"] = md["14"] + md["snsk.1"]
+    if (type == 'all' or 'snsk.3' in cols) and isvalid("21 22 23 24", md):
+        md["snsk.3"] = md["21"] + md["22"] + md["23"] + md["24"]
+    if (type == 'all' or 'snsk.4' in cols) and isvalid("20 snsk.3", md):
+        md["snsk.4"] = md["20"] + md["snsk.3"]
+    if (type == 'all' or 'snsk.5' in cols) and isvalid("snsk.2 snsk.4", md):
+        md["snsk.5"] = md["snsk.2"] + md["snsk.4"]
+    if (type == 'all' or 'snsk.6' in cols) and isvalid("snsk.5", md):
+        md["snsk.6"] = md["snsk.5"] / 2
+
+    if (type == 'all' or 'hin.1' in cols) and isvalid("27 28 29 30", md):
+        md["hin.1"] = md["27"] + md["28"] + md["29"] + md["30"]
+    if (type == 'all' or 'hin.2' in cols) and isvalid("26 hin.1", md):
+        md["hin.2"] = md["26"] + md["hin.1"]
+    if (type == 'all' or 'hin.3' in cols) and isvalid("33 34 35 36", md):
+        md["hin.3"] = md["33"] + md["34"] + md["35"] + md["36"]
+    if (type == 'all' or 'hin.4' in cols) and isvalid("32 hin.3", md):
+        md["hin.4"] = md["32"] + md["hin.3"]
+    if (type == 'all' or 'hin.5' in cols) and isvalid("hin.2 hin.4", md):
+        md["hin.5"] = md["hin.2"] + md["hin.4"]
+    if (type == 'all' or 'hin.6' in cols) and isvalid("hin.5", md):
+        md["hin.6"] = md["hin.5"] / 2
+
+    if (type == 'all' or 'eng.1' in cols) and isvalid("55 56 57 58", md):
+        md["eng.1"] = md["55"] + md["56"] + md["57"] + md["58"]
+    if (type == 'all' or 'eng.2' in cols) and isvalid("54 eng.1", md):
+        md["eng.2"] = md["54"] + md["eng.1"]
+    if (type == 'all' or 'eng.3' in cols) and isvalid("61 62 63 64", md):
+        md["eng.3"] = md["61"] + md["62"] + md["63"] + md["64"]
+    if (type == 'all' or 'eng.4' in cols) and isvalid("60 eng.3", md):
+        md["eng.4"] = md["60"] + md["eng.3"]
+    if (type == 'all' or 'eng.5' in cols) and isvalid("eng.2 eng.4", md):
+        md["eng.5"] = md["eng.2"] + md["eng.4"]
+    if (type == 'all' or 'eng.6' in cols) and isvalid("eng.5", md):
+        md["eng.6"] = md["eng.5"] / 2
+
+
