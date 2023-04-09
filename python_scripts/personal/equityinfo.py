313cdeb41f7e0879a3912747a392cfdfb18c6823
Dhaval Shah <seedhaval@gmail.com>
2023-04-09 11:46:47 +0530

    using nsepython and additional clauses

--- python_scripts/personal/equityinfo.py
+++ python_scripts/personal/equityinfo.py
@@ -1,16 +1,17 @@
-from nsepy.history import get_price_list
+from nsepython import get_bhavcopy
 from datetime import date
 import json
 
 dates = {
-    'latest': date(2023, 2, 22)
-    , 'prv_mo_1': date(2023, 1, 11)
-    , 'prv_mo_2': date(2023, 1, 25)
-    , 'six_mo_1': date(2022, 8, 10)
-    , 'six_mo_2': date(2022, 8, 23)
-    , 'prv_yr_1': date(2021, 12, 7)
-    , 'prv_yr_2': date(2021, 12, 21)
+    'latest': date(2023, 4, 6)
+    , 'prv_mo_1': date(2023, 3, 7)
+    , 'prv_mo_2': date(2023, 3, 14)
+    , 'six_mo_1': date(2022, 9, 29)
+    , 'six_mo_2': date(2022, 9, 5)
+    , 'prv_yr_1': date(2022, 1, 4)
+    , 'prv_yr_2': date(2022, 1, 28)
 }
+
 
 class Scrip:
     def __init__(self, d, nm):
@@ -24,17 +25,17 @@ class Scrip:
         self.prv_yr_2 = d['prv_yr_2'].get(nm, None)
 
         if self.prv_mo_1 and self.prv_mo_2:
-            self.prv_mo = (self.prv_mo_2+self.prv_mo_1)/2
+            self.prv_mo = (self.prv_mo_2 + self.prv_mo_1) / 2
         else:
             self.prv_mo = None
 
         if self.six_mo_1 and self.six_mo_2:
-            self.six_mo = (self.six_mo_1+self.six_mo_2)/2
+            self.six_mo = (self.six_mo_1 + self.six_mo_2) / 2
         else:
             self.six_mo = None
 
         if self.prv_yr_1 and self.prv_yr_2:
-            self.prv_yr = (self.prv_yr_1+self.prv_yr_2)/2
+            self.prv_yr = (self.prv_yr_1 + self.prv_yr_2) / 2
         else:
             self.prv_yr = None
 
@@ -48,14 +49,16 @@ def get_net_data():
     d = {}
 
     for k, v in dates.items():
-        print(k)
-        d[k] = {x[0]: x[5] for x in get_price_list(dt=v).values.tolist()}
+        print(k, v.strftime("%d-%m-%Y"))
+        d[k] = {x[0]: x[9] for x in get_bhavcopy(
+            date=v.strftime(
+                "%d-%m-%Y")).values.tolist()}
 
     with open('out.json', 'w') as f:
         f.write(json.dumps(d, indent=2))
 
 
-get_net_data()
+# get_net_data()
 scrip_ar = []
 
 with open('out.json', 'r') as f:
@@ -66,10 +69,25 @@ for k in d['latest'].keys():
     if s.valid:
         scrip_ar.append(s)
 
-out = []
-for s in scrip_ar:
-    if s.latest < 2000 and s.prv_yr < s.six_mo and s.prv_mo > s.latest:
-        out.append([s.nm, s.latest, s.prv_mo])
 
-for row in sorted(out, key=lambda x: (x[2] * 1.0) / x[1], reverse=True):
+def clause_1(scrip_ar):
+    out = []
+    for s in scrip_ar:
+        if s.latest < 5000 and s.prv_yr < s.six_mo and s.prv_mo > s.latest:
+            out.append([s.nm, s.latest, s.prv_mo, -(s.prv_mo * 1.0) / s.latest])
+    return out
+
+def clause_2(scrip_ar):
+    out = []
+    for s in scrip_ar:
+        if s.latest < 5000 \
+                and s.prv_yr < s.six_mo \
+                and s.six_mo > s.latest:
+            out.append([s.nm, s.latest, s.six_mo, -(s.six_mo * 1.0) / s.latest])
+    return out
+
+#out = clause_1(scrip_ar)
+out = clause_2(scrip_ar)
+
+for row in sorted(out, key=lambda x: x[3]):
     print(row)
