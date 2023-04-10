38c8d4c57379eada8c928b8878051178db4eae7c
Dhaval Shah <seedhaval@gmail.com>
2023-04-10 16:09:10 +0530

    fit 2 report card in one A4 page

--- result_9th/lib/individual_result.py
+++ result_9th/lib/individual_result.py
@@ -10,27 +10,27 @@ from lib.uihelper import MyApp
 d = {}
 
 
-def draw_excel_populate_marks(wb, ir, student):
+def draw_excel_populate_marks(wb, ir, student, left):
     sid, roll, nm = student
     md = d['marksMap'][sid]
     sht = wb.active
-    Cell(ir + 6, 3, sht).set(roll)
-    Cell(ir + 6, 6, sht).set("तुकडी - " + d['ddDivision'].get())
-    Cell(ir + 7, 4, sht).set(nm)
-    Cell(ir + 9, 6, sht).set(md['fin.mar.r1'])
-    Cell(ir + 10, 6, sht).set(md['fin.hin.r1'])
-    Cell(ir + 11, 6, sht).set(md['fin.eng.r1'])
-    Cell(ir + 12, 6, sht).set(md['fin.grp.l1'])
-    Cell(ir + 13, 6, sht).set(md['fin.mat.r1'])
-    Cell(ir + 14, 6, sht).set(md['fin.sci.r1'])
-    Cell(ir + 15, 6, sht).set(md['fin.grp.l2'])
-    Cell(ir + 16, 6, sht).set(md['fin.smj.r1'])
-    Cell(ir + 17, 6, sht).set(md['fin.aro.l1'])
-    Cell(ir + 18, 6, sht).set(md['fin.jals.l1'])
-    Cell(ir + 19, 6, sht).set(md['fin.ncc.l1'])
-    Cell(ir + 20, 6, sht).set(md['fin.total.l1'])
-    Cell(ir + 21, 6, sht).set(md['fin.100.l1'])
-    Cell(ir + 22, 4, sht).set(md['final.pass.status'])
+    Cell(ir + 4, left + 1, sht).set(roll)
+    Cell(ir + 4, left + 4, sht).set("तुकडी - " + d['ddDivision'].get())
+    Cell(ir + 5, left + 2, sht).set(nm)
+    Cell(ir + 7, left + 4, sht).set(md['fin.mar.r1'])
+    Cell(ir + 8, left + 4, sht).set(md['fin.hin.r1'])
+    Cell(ir + 9, left + 4, sht).set(md['fin.eng.r1'])
+    Cell(ir + 10, left + 4, sht).set(md['fin.grp.l1'])
+    Cell(ir + 11, left + 4, sht).set(md['fin.mat.r1'])
+    Cell(ir + 12, left + 4, sht).set(md['fin.sci.r1'])
+    Cell(ir + 13, left + 4, sht).set(md['fin.grp.l2'])
+    Cell(ir + 14, left + 4, sht).set(md['fin.smj.r1'])
+    Cell(ir + 15, left + 4, sht).set(md['fin.aro.l1'])
+    Cell(ir + 16, left + 4, sht).set(md['fin.jals.l1'])
+    Cell(ir + 17, left + 4, sht).set(md['fin.ncc.l1'])
+    Cell(ir + 18, left + 4, sht).set(md['fin.total.l1'])
+    Cell(ir + 19, left + 4, sht).set(md['fin.100.l1'])
+    Cell(ir + 20, left + 2, sht).set(md['final.pass.status'])
 
 
 
@@ -55,16 +55,21 @@ def get_output_file_path():
 def add_excel_result(wb):
     sht = wb.active
     for i, student in enumerate(d['studentMap']):
-        ir = (i * 37) + 1
-        apply_template(sht, d['tmplt'], 2, ir)
-        draw_excel_populate_marks(wb, ir, student)
-        add_page_break(sht, ir + 35)
+        ir = ((i//2) * 29) + 1
+        print(ir)
+        left = 2 if i%2 == 0 else 9
+        apply_template(sht, d['tmplt'], left, ir)
+        draw_excel_populate_marks(wb, ir, student, left)
+        if i%2 == 1:
+            add_page_break(sht, ir + 28)
 
 
 def populate_excel(wb):
     sht = wb.active
+    sht.set_printer_settings(paper_size=sht.PAPERSIZE_A4,
+                             orientation='landscape')
     load_data()
-    apply_column_widths(sht, 1, [5, 10, 20, 12, 11, 12, 12])
+    apply_column_widths(sht, 1, [2, 8, 15, 7, 7, 7, 10, 4, 8, 15, 7, 7, 7, 10])
     add_excel_result(wb)
 
 
